f9b9d4e49939bc6625b3d4127de75c49c491b4bd
Jara <jara@sourware.com>
2024-10-07 22:14:19 +0200

    refactor: Use target_include_directories

--- deps/stb.cmake
+++ deps/stb.cmake
@@ -1,5 +1,7 @@
+set(INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/external)
+
 add_library(stb_image SHARED ${CMAKE_SOURCE_DIR}/deps/stb/stb_image_implementation.cxx)
-target_include_directories(stb_image PUBLIC ${CMAKE_SOURCE_DIR}/external/stb)
+target_include_directories(stb_image PUBLIC ${INCLUDE_DIRS})
 
 add_library(stb_perlin SHARED ${CMAKE_SOURCE_DIR}/deps/stb/stb_perlin_implementation.cxx)
-target_include_directories(stb_perlin PUBLIC ${CMAKE_SOURCE_DIR}/external/stb)
+target_include_directories(stb_perlin PUBLIC ${INCLUDE_DIRS})
